10ece68fc955e3416da7cc9492d5a1a10a5398e7
rakaramadhani <rakaramadhani2001@gmail.com>
2025-03-27 14:49:12 +0700

    vercel.json config

--- src/server.js
+++ src/server.js
@@ -1,41 +1,44 @@
 const express = require('express');
-const { createServer } = require('@vercel/node'); // Import handler untuk Vercel
 const dotenv = require('dotenv');
 dotenv.config();
-const cors = require('cors');
-const adminAuthRoutes = require("../src/routes/admin/adminAuthRoutes");
-const adminRoutes = require("../src/routes/admin/adminRoutes");
-const userAuthRoutes = require("../src/routes/penghuni/penghuniAuthRoutes");
-const userRoutes = require("../src/routes/penghuni/penghuniRoutes");
-const seeder = require("../prisma/seed");
+const  cors = require('cors')
+const app = express();
+const adminAuthRoutes = require("./routes/admin/adminAuthRoutes");
+const adminRoutes = require("./routes/admin/adminRoutes");
+const userAuthRoutes = require("./routes/penghuni/penghuniAuthRoutes");
+const userRoutes = require("./routes/penghuni/penghuniRoutes");
+const seeder = require("../prisma/seed")
+
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const app = express();
+app.use(express.json())
+app.use(cors())
 
-app.use(express.json());
-app.use(cors());
 
 app.use("/api", adminAuthRoutes);
 app.use("/api", adminRoutes);
-app.use("/api", userAuthRoutes);
-app.use("/api", userRoutes);
+app.use("/api",userAuthRoutes);
+app.use("/api",userRoutes);
 
-async function initializeSeeder() {
-  try {
-    await seeder();
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await prisma.$disconnect();
-  }
-}
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send({ error: "Internal Server Error" });
+const PORT = process.env.PORT;
+
+async function startServer() {
+  // Ambil seeder
+  await seeder()
+      .catch((e) => {
+          console.error(e);
+          process.exit(1);
+      })
+      .finally(async () => {
+          await prisma.$disconnect();
+      });
+
+  // Port
+  app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
   });
-// Jalankan seeder sebelum menerima request
-initializeSeeder();
-
-module.exports = createServer(app); // Ekspor server untuk Vercel
+}
+  
+startServer();
